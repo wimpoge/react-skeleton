d9a556a65defc3effb3710497a22f5fdcbded95c
Muhamad Rafli <34478110+wimpoge@users.noreply.github.com>
2022-10-03 21:44:50 +0700

    push

---
+++ src/components/User.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import SkeletonProfile from "../skeletons/SkeletonProfile";
+
+
+const User = () => {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+      const data = await res.json();
+      setProfile(data);
+    }, 5000);
+  });
+  return (
+    <div className="user">
+      <h2>User</h2>
+      {profile && (
+        <div className="profile" key={profile.id}>
+        <h3>{profile.username}</h3>
+        <p>{profile.email}</p>
+        <a href={profile.website}>{profile.website}</a>
+        </div>
+        
+      )}
+
+      {!profile && <SkeletonProfile  theme="dark"/>}
+    </div>
+  );
+};
+
+export default User;
